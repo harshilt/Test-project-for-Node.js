6856f2094da10925d91f823adc1737687c9f4200
harshilt <harshilstrivedi@gmail.com>
2016-05-25 17:06:39 +0530

    solution created

---
+++ Nightwatch project/Nightwatch project/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nightwatch_project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
